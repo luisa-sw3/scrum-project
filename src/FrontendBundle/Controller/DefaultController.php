2e655fc1b6a86040bde2b8e7dc3c5988756a604c
cesar-giraldo <cesargiraldo1108@gmail.com>
2016-01-18 18:45:02 -0500

    Maqueta base para el frontend

--- src/FrontendBundle/Controller/DefaultController.php
+++ src/FrontendBundle/Controller/DefaultController.php
@@ -4,10 +4,22 @@ namespace FrontendBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function indexAction()
-    {
+class DefaultController extends Controller {
+
+    public function indexAction() {
         return $this->render('FrontendBundle:Default:index.html.twig');
     }
+    
+    public function aboutAction() {
+        return $this->render('FrontendBundle:Default:about.html.twig');
+    }
+    
+    public function samplePostAction() {
+        return $this->render('FrontendBundle:Default:samplePost.html.twig');
+    }
+    
+    public function contactAction() {
+        return $this->render('FrontendBundle:Default:contact.html.twig');
+    }
+
 }
